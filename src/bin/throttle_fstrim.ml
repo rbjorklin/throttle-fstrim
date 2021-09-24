6a4f9c7a0b9da57333f6ee7f15bd9fee008182d6
Robin Björklin <robin.bjorklin@gmail.com>
2021-09-23 19:29:04 -0700

    fix: add command line arguments

--- src/bin/throttle_fstrim.ml
+++ src/bin/throttle_fstrim.ml
@@ -7,20 +7,43 @@ let timestamp_tag : Logs.Tag.def =
 
 (* let timestamp c = Logs.Tag.(empty |> add timestamp_tag (Mtime_clock.count c)) *)
 
-let length =
-  let doc = "Number of bytes to trim in each execution." in
-  Arg.(value & opt int 64000000 & info [ "l"; "length" ] ~docv:"LENGTH" ~doc)
+type config = {
+  length : int;
+  max_queue : float;
+}
 
-let max_queue =
-  let doc =
-    "Max queue size allowed before starting to sleep between executions."
+let length_term =
+  let default = 64000000 in
+  let info =
+    Arg.info
+      [ "l"; "length" ] (* '-U' and '--username' will be synonyms *)
+      ~docv:"LENGTH" ~doc:"Number of bytes to trim in each execution."
   in
-  Arg.(value & opt float 1. & info [ "m"; "max-queue" ] ~docv:"MAX_QUEUE" ~doc)
+  Arg.value (Arg.opt Arg.int default info)
 
-let info =
-  let doc = "Run fstrim but with rudimentary throttling." in
-  Term.info "throttle-fstrim" ~version:"%%VERSION%%" ~doc
-    ~exits:Term.default_exits
+let max_queue_term =
+  let default = 1. in
+  let info =
+    Arg.info
+      [ "q"; "max-queue" ] (* '-U' and '--username' will be synonyms *)
+      ~docv:"QUEUE"
+      ~doc:"Max queue size allowed before starting to sleep between executions."
+  in
+  Arg.value (Arg.opt Arg.float default info)
+
+let conf_term =
+  let combine length max_queue = { length; max_queue } in
+  Term.(const combine $ length_term $ max_queue_term)
+
+let parse_command_line () =
+  let info =
+    Term.info "throttle-fstrim"
+    (* program name as it will appear in --help *)
+  in
+  match Term.eval (conf_term, info) with
+  | `Error _ -> exit 1
+  | `Version | `Help -> exit 0
+  | `Ok conf -> conf
 
 type mount = {
   device : string;
@@ -155,6 +178,6 @@ let main length max_queue =
   Logs.info (fun m -> m "test");
   read_mounts () |> fstrim ~length ~max_queue
 
-let throttle_fstrim_t = Term.(const main $ length $ max_queue)
-
-let () = Term.exit @@ Term.eval (throttle_fstrim_t, info)
+let () =
+  let conf = parse_command_line () in
+  main conf.length conf.max_queue
